961417df9d61b6793d913c042f1b05d54e9345e2
Muneeb Sultan <muhammadmuneebsultan222@gmail.com>
2024-07-02 18:50:30 +0500

    commit

--- src/components/Navbarsecond.js
+++ src/components/Navbarsecond.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import '../assets/css/navbarsecond.css';
 
 const Navbarsecond = () => {
   return (
-    <div>
-      <nav>
-<button className="btn btn-success d-flex "><i className="bi bi-border-all"></i> Departments</button>
-<ul>
-<li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
+    <div className='container'>
+      <nav className=' d-flex'>
+<button className="btn btn-success d-flex pl-2"><i className="bi bi-grid"></i>Departments</button>
+<ul className='d-flex justify-content-between gap-3'>
+<li className="nav-item dropdown nav-li">
+          <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Home
           </a>
           <ul className="dropdown-menu">
             <li><a className="dropdown-item" href="/">Action</a></li>
@@ -16,27 +17,27 @@ const Navbarsecond = () => {
             <li><a className="dropdown-item" href="/">Something else here</a></li>
           </ul>
         </li>
-        <li className="nav-item dropdown">
+        <li className="nav-item dropdown nav-li">
           <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
+            Shop
           </a>
           <ul className="dropdown-menu">
             <li><a className="dropdown-item" href="/">Action</a></li>
             <li><a className="dropdown-item" href="/">Another action</a></li>
             <li><a className="dropdown-item" href="/">Something else here</a></li>
           </ul>
-        </li><li className="nav-item dropdown">
+        </li><li className="nav-item dropdown nav-li">
           <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
+            Stores
           </a>
           <ul className="dropdown-menu">
             <li><a className="dropdown-item" href="/">Action</a></li>
             <li><a className="dropdown-item" href="/">Another action</a></li>
             <li><a className="dropdown-item" href="/">Something else here</a></li>
           </ul>
-        </li><li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
+        </li><li className="nav-item dropdown nav-li">
+          <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Mega menu
           </a>
           <ul className="dropdown-menu">
             <li><a className="dropdown-item" href="/">Action</a></li>
@@ -44,9 +45,9 @@ const Navbarsecond = () => {
             <li><a className="dropdown-item" href="/">Something else here</a></li>
           </ul>
         </li>
-        <li className="nav-item dropdown">
+        <li className="nav-item dropdown nav-li">
           <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
+            Pages
           </a>
           <ul className="dropdown-menu">
             <li><a className="dropdown-item" href="/">Action</a></li>
@@ -54,9 +55,9 @@ const Navbarsecond = () => {
             <li><a className="dropdown-item" href="/">Something else here</a></li>
           </ul>
         </li>
-        <li className="nav-item dropdown">
+        <li className="nav-item dropdown nav-li">
           <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
+            Account
           </a>
           <ul className="dropdown-menu">
             <li><a className="dropdown-item" href="/">Action</a></li>
@@ -64,6 +65,9 @@ const Navbarsecond = () => {
             <li><a className="dropdown-item" href="/">Something else here</a></li>
           </ul>
         </li>
+        <li className='nav-li'>Dashboard</li>
+        <li className='nav-li'>Docs</li>
+
 </ul>
 
 
